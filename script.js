27b56f4dd04b7dff8d1aebb5d326945a5aaee4fd
harsh-pandhe <harshpandhehome@gmail.com>
2024-01-26 14:13:27 +0530

    attempt

---
+++ script.js
@@ -0,0 +1,7 @@
+import { EditorView, basicSetup } from "./node_modules/@codemirror"
+import { javascript } from "./node_modules/@codemirror/lang-javascript"
+
+let editor = new EditorView({
+    extensions: [basicSetup, javascript()],
+    parent: document.body
+})
